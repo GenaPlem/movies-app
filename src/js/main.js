06421571a22030487f6d94fbc425e54a1cd758e3
GenaPlem <shainkill1337@gmail.com>
2022-10-28 18:23:47 +0300

    add func getData and renderMovies, add some styles and html structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
 export const {API_KEY} = process.env;
+
+const getData = (request, selector) => {
+    const url = 'https://api.themoviedb.org/3';
+    return fetch(`${url}${request}?api_key=${API_KEY}`)
+    .then(res => res.json())
+    .then(data => renderMovies(data, selector))
+    
+}
+
+const renderMovies = (data, selector) => {
+    let res = data.results.map(({ poster_path, original_title, popularity, release_date}) => (
+    `<div class="${selector}__movie">
+        <img src="https://image.tmdb.org/t/p/w200/${poster_path}" alt="${original_title}>"<br>
+        <div class="${selector}__title">${original_title}</div>
+        <span class="${selector}__rate">${popularity}</span>
+        <div class="${selector}__date">${release_date}</div>
+    </div>`
+    )).join('');
+
+    document.querySelector('.'+selector).innerHTML += res;
+    }
+
+getData('/movie/popular', 'popular')
+
+getData('/movie/top_rated', 'top_rated')
+
+getData('/movie/upcoming', 'upcoming')
