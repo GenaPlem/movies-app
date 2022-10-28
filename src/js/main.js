a5628d9e79ec9b009f85f22d005c9c24245943cf
GenaPlem <shainkill1337@gmail.com>
2022-10-28 18:56:33 +0300

    add func to formate date

--- src/js/main.js
+++ src/js/main.js
@@ -13,12 +13,21 @@ const renderMovies = (data, selector) => {
         <img src="https://image.tmdb.org/t/p/w200/${poster_path}" alt="${original_title}>"<br>
         <div class="${selector}__title">${original_title}</div>
         <span class="${selector}__rate">${popularity}</span>
-        <div class="${selector}__date">${release_date}</div>
+        <div class="${selector}__date">${formateDate(release_date)}</div>
     </div>`
     )).join('');
 
     document.querySelector('.'+selector).innerHTML += res
     }
+
+const formateDate = (date) => {
+    let release = new Date(date)
+    return release.toLocaleDateString('en-GB', {
+    day: '2-digit', month: 'short', year: 'numeric'
+    }).replace(/ /g, '-');
+    
+}
+
 
 getData('/movie/popular')
 .then(data => renderMovies(data, 'popular'))
